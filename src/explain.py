306ffa6f3a80c22c5fb41e2ce90d26871037f3cc
Martin Pan <wpan03@email.wm.edu>
2021-07-06 10:01:43 -0500

    add functions for logistic regression

--- src/explain.py
+++ src/explain.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
+
+def get_lg_coef(df: pd.DataFrame, mod_lg) -> pd.DataFrame:
+    """Get and sort the coefficent from logistic regression"""
+    df_coef = pd.DataFrame({'name': df.columns,
+                            'value': mod_lg.coef_})
+    return df_coef.sort_values('value', ignore_index=True)
+
+
 def get_feature_imp(df: pd.DataFrame, mod_rf) -> pd.DataFrame:
     """Get the feature importance of random forest model"""
-    result = pd.DataFrame({'name':df.columns,
-                          'score':mod_rf.feature_importances_})
-    
+    result = pd.DataFrame({'name': df.columns,
+                           'score': mod_rf.feature_importances_})
+
     return result.sort_values('score', ascending=False, ignore_index=True)
