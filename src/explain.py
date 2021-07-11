1e89e5f8e413522b079609217f07e6b8e19fdada
Martin Pan <wpan03@email.wm.edu>
2021-07-11 10:49:49 -0500

    add error analysis

--- src/explain.py
+++ src/explain.py
@@ -47,7 +47,6 @@ def get_permute_imp_df(mod, X: pd.DataFrame, y: Union[pd.Series, np.ndarray],
     return df_result
 
 
-
 # Ref: https://scikit-learn.org/stable/auto_examples/inspection/plot_permutation_importance.html
 def plot_permute_imp(X: pd.DataFrame, result) -> None:
     """Make a box plot from the permutation importance result"""
@@ -59,3 +58,26 @@ def plot_permute_imp(X: pd.DataFrame, result) -> None:
     ax.set_title("Permutation Importances (test set)")
     fig.tight_layout()
     plt.show()
+
+
+def cross_entropy_loss(y_true: np.ndarray, y_pred_1: np.ndarray, eps: float = 1e-8) -> np.ndarray:
+    """Calculate the binary cross entropy loss for each observation, no reduction"""
+    y_pred_1 = np.clip(y_pred_1, eps, 1 - eps)
+    loss = -(y_true * np.log(y_pred_1) + (1 - y_true) * np.log(1 - y_pred_1))
+    return loss
+
+
+def get_binary_error_analysis(X: pd.DataFrame, y: pd.Series, mod, only_show_error: bool = False) -> pd.DataFrame:
+    """Calculate the loss for each row withe actual label and predicted label"""
+    df_result = X.copy(deep=True)
+    pred_prob = mod.predict_proba(X)
+    pred_class = mod.predict(X)
+    loss = cross_entropy_loss(y.values, pred_prob[:, 1])
+    df_result['class1_prob'] = pred_prob[:, 1]
+    df_result['pred_class'] = pred_class
+    df_result['actual_class'] = y.values
+    df_result['loss'] = loss
+    if only_show_error:
+        df_result = df_result[df_result['pred_class']
+                              != df_result['actual_class']]
+    return df_result.sort_values('loss', ascending=False, ignore_index=True)
