0aa077b20dbcaa2bc1c148266cd52b40bdb18b4d
Martin Pan <wpan03@email.wm.edu>
2021-07-17 09:18:59 -0500

    introduce balanced accuracy

--- src/evaluate.py
+++ src/evaluate.py
@@ -16,13 +16,14 @@ def binary_evaluation(X: Union[pd.DataFrame, np.ndarray],
     pred = mod.predict(X)
 
     acc = metrics.accuracy_score(y, pred)
+    bal_acc = metrics.balanced_accuracy_score(y, pred)
     prec = metrics.precision_score(y, pred)
     rec = metrics.recall_score(y, pred)
     f1 = metrics.f1_score(y, pred)
 
     df_result = pd.DataFrame(columns=['metrics', 'value'])
-    df_result['metrics'] = ['accuracy', 'precision', 'recall', 'f1']
-    df_result['value'] = [acc, prec, rec, f1]
+    df_result['metrics'] = ['accuracy', 'balanced acc', 'precision', 'recall', 'f1']
+    df_result['value'] = [acc, bal_acc, prec, rec, f1]
 
     return df_result
 
@@ -50,11 +51,13 @@ def binary_cross_evaluate(X: Union[pd.DataFrame, np.ndarray],
         clf = make_pipeline(preprocessor, mod)
     else:
         clf = mod
-    result_dict = cross_validate(clf, X, y, cv=num_fold, scoring=['accuracy', 'f1'], **kwargs)
+    result_dict = cross_validate(clf, X, y, cv=num_fold, scoring=[
+                                 'accuracy', 'f1'], **kwargs)
     return pd.DataFrame(result_dict)
 
+
 def plot_cf(mod,
-            X: Union[pd.DataFrame, np.ndarray], 
+            X: Union[pd.DataFrame, np.ndarray],
             y: Union[pd.Series, np.ndarray],
             title: int = 'Confusion Matrix') -> None:
     disp = metrics.plot_confusion_matrix(mod, X, y, cmap=plt.cm.Blues)
