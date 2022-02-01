48f2d41f320a6b5ee9ff1c33f3ad73e86851a57d
Martin Pan <wpan03@email.wm.edu>
2022-02-01 15:15:59 -0500

    ✅ add 1 test

--- test/test_eda.py
+++ test/test_eda.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from src.eda import see_missing
+from src.eda import see_missing, get_freq_table
 
 
 def test_see_missing():
@@ -20,4 +20,12 @@ def test_see_missing():
             "percent_missing": [50.0, 25.0, 0.0],
         }
     )
+    pd.testing.assert_frame_equal(df_true, df_test)
+
+
+def test_get_freq_table():
+    df_in = pd.DataFrame({"col1": ["a"] * 2 + ["b"] * 3, "col2": [1] * 5})
+    df_test = get_freq_table(df_in, col="col1")
+    df_true = pd.DataFrame({"count": [3, 2], "percentage": [0.6, 0.4]})
+    df_true.index = ["b", "a"]
     pd.testing.assert_frame_equal(df_true, df_test)
