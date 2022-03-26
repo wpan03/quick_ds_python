9ea863092bef8c8b0c3b98b67f34f62e7f66fb03
Martin Pan <wpan03@email.wm.edu>
2022-03-26 10:31:41 -0700

    fix few default

--- nice_things/a_b_test.py
+++ nice_things/a_b_test.py
@@ -3,16 +3,15 @@ from math import ceil
 
 
 def get_sample_size_for_prop_test(
-    effect_size: float,
-    base_rate: float = 0.01,
+    base_rate: float,
+    target_rate: float,
     alpha: float = 0.01,
-    power: float = 0.8,
+    the_power: float = 0.8,
     ratio: float = 1.0,
 ) -> int:
     """Calculate the Required Sample Size for Proportion Test"""
-    effect_size = sms.proportion_effectsize(base_rate, base_rate + effect_size)
-
+    effect_size = sms.proportion_effectsize(base_rate, target_rate)
     required_n = sms.NormalIndPower().solve_power(
-        effect_size, power=power, alpha=alpha, ratio=ratio
+        effect_size, power=the_power, alpha=alpha, ratio=ratio
     )
     return ceil(required_n)
